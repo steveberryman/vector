fd990dc7588bcf2a62249debdfa67bff9569f2ab
Jean Mertz <git@jeanmertz.com>
2021-04-19 15:06:49 +0200

    tmp3
    Signed-off-by: Jean Mertz <git@jeanmertz.com>

--- src/sinks/vector_grpc.rs
+++ src/sinks/vector_grpc.rs
@@ -1,18 +1,19 @@
+use crate::sinks::util::{BatchConfig, BatchSettings, BatchSink, EncodedLength, VecBuffer};
 use crate::{
     config::{DataType, GenerateConfig, SinkConfig, SinkContext, SinkDescription},
     event::proto as event,
-    sinks::{Healthcheck, Sink, VectorSink},
+    sinks::{util::sink, Healthcheck, VectorSink},
     Event,
 };
-use futures::{Future, FutureExt};
-use getset::Setters;
-use serde::{Deserialize, Serialize};
-use std::pin::Pin;
-use std::task::{Context, Poll};
-use tonic::{
-    client::GrpcService,
-    transport::{Channel, Endpoint},
+use futures::{
+    future::{self, BoxFuture},
+    stream, SinkExt, StreamExt, TryFutureExt,
 };
+use getset::Setters;
+use prost::Message;
+use serde::{Deserialize, Serialize};
+use std::task::{Context, Poll};
+use tonic::{transport::Endpoint, IntoRequest};
 
 // TODO: duplicated for sink/source, should move to util.
 mod proto {
@@ -22,16 +23,17 @@ mod proto {
     include_proto!("vector");
 }
 
+type Client = proto::Client<tonic::transport::Channel>;
+type Request = tonic::Request<proto::EventRequest>;
+type Response = Result<tonic::Response<proto::EventAck>, tonic::Status>;
+
 #[derive(Deserialize, Serialize, Debug, Setters)]
 #[serde(deny_unknown_fields)]
 pub struct VectorConfig {
     address: String,
     // TODO
-    // #[serde(default)]
-    // pub compression: Compression,
-    // pub encoding: EncodingConfig<Encoding>,
-    // #[serde(default)]
-    // pub batch: BatchConfig,
+    #[serde(default)]
+    pub batch: BatchConfig,
     // #[serde(default)]
     // pub request: RequestConfig,
     // pub tls: Option<TlsOptions>,
@@ -39,7 +41,10 @@ pub struct VectorConfig {
 
 impl VectorConfig {
     pub fn new(address: String) -> Self {
-        Self { address }
+        Self {
+            address,
+            batch: BatchConfig::default(),
+        }
     }
 }
 
@@ -56,13 +61,25 @@ impl GenerateConfig for VectorConfig {
 #[async_trait::async_trait]
 #[typetag::serde(name = "vector_grpc")]
 impl SinkConfig for VectorConfig {
-    async fn build(&self, _cx: SinkContext) -> crate::Result<(VectorSink, Healthcheck)> {
+    async fn build(&self, cx: SinkContext) -> crate::Result<(VectorSink, Healthcheck)> {
         let endpoint = Endpoint::from_shared(self.address.clone()).expect("TODO");
         let channel = endpoint.connect_lazy().expect("TODO");
-        let sink = GrpcSink {
-            channel,
-            future: None,
-        };
+        let client = proto::Client::new(channel);
+
+        let batch = BatchSettings::default()
+            .bytes(1300)
+            .events(1000)
+            .timeout(1)
+            .parse_config(self.batch)?;
+
+        let sink = BatchSink::new(
+            client,
+            VecBuffer::new(batch.size),
+            batch.timeout,
+            cx.acker(),
+        )
+        .sink_map_err(|error| error!(message = "Fatal GRPC sink error.", %error))
+        .with_flat_map(move |event| stream::iter(encode_event(event)).map(Ok));
 
         // TODO
         let healthcheck = async move { Ok(()) };
@@ -79,60 +96,57 @@ impl SinkConfig for VectorConfig {
     }
 }
 
-struct GrpcSink {
-    channel: Channel,
-    future: Option<
-        Pin<
-            Box<
-                dyn Future<Output = Result<tonic::Response<proto::EventAck>, tonic::Status>> + Send,
-            >,
-        >,
-    >,
+fn encode_event(event: Event) -> Option<Request> {
+    let request = proto::EventRequest {
+        message: Some(event.into()),
+    };
+
+    Some(request.into_request())
 }
 
-impl Sink<Event> for GrpcSink {
-    type Error = ();
+impl EncodedLength for Request {
+    fn encoded_length(&self) -> usize {
+        self.get_ref().encoded_len()
+    }
+}
 
-    fn poll_ready(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Result<(), Self::Error>> {
-        if self.future.is_some() {
-            return Poll::Pending;
+impl tower::Service<Vec<Request>> for Client {
+    type Response = ();
+    type Error = String;
+    type Future = BoxFuture<'static, Result<Self::Response, Self::Error>>;
+
+    fn poll_ready(&mut self, _cx: &mut Context<'_>) -> Poll<Result<(), Self::Error>> {
+        Poll::Ready(Ok(()))
+    }
+
+    fn call(&mut self, requests: Vec<Request>) -> Self::Future {
+        let mut futures = Vec::with_capacity(requests.len());
+
+        for request in requests {
+            let mut client = self.clone();
+            futures.push(async move {
+                client
+                    .push_events(request)
+                    .map_err(|err| err.to_string())
+                    .await
+            })
         }
 
-        self.channel.poll_ready(cx).map_err(|_| ())
+        Box::pin(async move {
+            future::join_all(futures)
+                .await
+                .into_iter()
+                .map(|v| match v {
+                    Ok(..) => Ok(()),
+                    Err(e) => Err(e.to_string()),
+                })
+                .collect::<Result<_, _>>()
+        })
     }
+}
 
-    fn start_send(mut self: Pin<&mut Self>, item: Event) -> Result<(), Self::Error> {
-        let channel = self.channel.clone();
-        let future = Box::pin(async move {
-            let mut client = proto::Client::new(channel);
-            let request = proto::EventRequest {
-                message: Some(item.into()),
-            };
-
-            client.push_events(request).await
-        });
-
-        self.future = Some(future);
-
-        Ok(())
-    }
-
-    fn poll_flush(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Result<(), Self::Error>> {
-        match self.future.take() {
-            Some(mut fut) => match fut.poll_unpin(cx) {
-                Poll::Pending => {
-                    self.future.replace(fut);
-                    return Poll::Pending;
-                }
-                _ => {}
-            },
-            None => {}
-        };
-
-        Poll::Ready(Ok(()))
-    }
-
-    fn poll_close(self: Pin<&mut Self>, _: &mut Context<'_>) -> Poll<Result<(), Self::Error>> {
-        Poll::Ready(Ok(()))
+impl sink::Response for Response {
+    fn is_successful(&self) -> bool {
+        self.is_ok()
     }
 }
